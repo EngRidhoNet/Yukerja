2d9f7480e03f487c5e6984ae6270031755ede4bb
yudhapramanaputra13 <yudhapramanaputra58@gmail.com>
2025-04-21 21:12:21 +0700

    add users crud

---
+++ app/Models/MitraPortfolio.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MitraPortfolio extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'mitra_id',
+        'title',
+        'description',
+        'image_url',
+        'completion_date',
+    ];
+
+    public function mitra()
+    {
+        return $this->belongsTo(Mitra::class);
+    }
+}
